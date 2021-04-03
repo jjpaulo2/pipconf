b6a2df169be8d67f17e4775bd8fd06fc0ca691f7
João Paulo Carvalho <joaopaulo.carvalho@br.experian.com>
2021-04-03 20:03:14 -0300

    tests sketch

---
+++ tests/test_reader.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+from pipconf import reader
+
+class ReaderFunctionsTestCase(TestCase):
+
+    def test_configuration_files_filter(self):
+        ...
+
+    def test_get_configuration_files(self):
+        ...
+
+    def test_get_user_configuration_files(self):
+        ...
+
+    def test_get_local_configuration_files(self):
+        ...
+    
---
+++ tests/test_switch.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+from pipconf import switch
+
+class SwitcherFunctionsTestCase(TestCase):
+
+    def test_get_current_configuration(self):
+        ...
+
+    def test_remove_current_configuration(self):
+        ...
+
+    def test_set_current_configuration(self):
+        ...
